34d51b9a1a2a5bc9132884051bdbf8c518bf47f3
rajarshimaitra <rajarshi149@gmail.com>
2021-12-12 22:45:09 +0530

    Add basic mod and rpc verification test

--- src/main.rs
+++ src/main.rs
@@ -66,8 +66,6 @@ fn main() {
     let rpc_config = RpcConfig {
         url: rpc_url,
         auth,
-        proxy: Some("127.0.0.1:9050".to_string()),
-        proxy_auth: None,
         network: Network::Regtest,
         wallet_name,
         skip_blocks: None,
@@ -164,13 +162,13 @@ fn get_descriptors() -> (String, String) {
     let xkey: ExtendedKey = (mnemonic, password).into_extended_key().unwrap();
     let xprv = xkey.into_xprv(Network::Regtest).unwrap();
 
-    // Derive our dewscriptors to use
+    // Derive our descriptors to use
     // We use the following paths for recieve and change descriptor
-    // recieve: "m/84h/1h/0h/0"
+    // receive: "m/84h/1h/0h/0"
     // change: "m/84h/1h/0h/1"
     let mut keys = Vec::new();
 
-    for path in ["m/84h/1h/0h/0", "m/84h/1h/0h/1"] {
+    for path in ["m/84h/1h/0h/0", "m/84h/1h/0h/1"].iter() {
         let deriv_path: DerivationPath = DerivationPath::from_str(path).unwrap();
         let derived_xprv = &xprv.derive_priv(&secp, &deriv_path).unwrap();
         let origin: KeySource = (xprv.fingerprint(&secp), deriv_path);
@@ -189,4 +187,151 @@ fn get_descriptors() -> (String, String) {
 
     // Return the keys as a tupple
     (keys[0].clone(), keys[1].clone())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use bdk::bitcoin::consensus::encode::deserialize;
+    use bdk::bitcoin::Transaction;
+    use bdk::bitcoincore_rpc::json::CreateRawTransactionInput;
+    use bdk::wallet::verify::verify_tx;
+    use std::collections::HashMap;
+
+    #[test]
+    fn test_transaction_validation() {
+        // Create a RPC interface
+        let rpc_auth = rpc_auth::UserPass("admin".to_string(), "passw".to_string());
+        let core_rpc =
+            Client::new("http://127.0.0.1:18443/wallet/test-verification", rpc_auth).unwrap();
+
+        // Create the test wallet
+        core_rpc
+            .create_wallet("test-verification", None, None, None, None)
+            .unwrap();
+
+        // Get a new address
+        let core_coinbase_addr = core_rpc.get_new_address(None, None).unwrap();
+
+        // Generate 101 blocks and use the above address as coinbase
+        core_rpc
+            .generate_to_address(101, &core_coinbase_addr)
+            .unwrap();
+
+        // -----------
+        // Create BDK wallet
+        // Get receive and change descriptor
+        let (receive_desc, change_desc) = get_descriptors();
+
+        // Use deterministic wallet name derived from descriptor
+        let wallet_name = wallet_name_from_descriptor(
+            &receive_desc,
+            Some(&change_desc),
+            Network::Regtest,
+            &Secp256k1::new(),
+        )
+        .unwrap();
+
+        // Create the datadir to store wallet data
+        let mut datadir = dirs_next::home_dir().unwrap();
+        datadir.push(".bdk-example");
+        let database = sled::open(datadir).unwrap();
+        let db_tree = database.open_tree(wallet_name.clone()).unwrap();
+
+        // Set RPC username and password
+        let auth = Auth::UserPass {
+            username: "admin".to_string(),
+            password: "passw".to_string(),
+        };
+
+        // Set RPC url
+        let mut rpc_url = "http://".to_string();
+        rpc_url.push_str("127.0.0.1:18443");
+
+        // Setup the RPC configuration
+        let rpc_config = RpcConfig {
+            url: rpc_url,
+            auth,
+            network: Network::Regtest,
+            wallet_name,
+            skip_blocks: None,
+        };
+
+        // Use the above configuration to create a RPC blockchain backend
+        let blockchain = RpcBlockchain::from_config(&rpc_config).unwrap();
+
+        // Combine everything and finally create the BDK wallet structure
+        let wallet = Wallet::new(
+            &receive_desc,
+            Some(&change_desc),
+            Network::Regtest,
+            db_tree,
+            blockchain,
+        )
+        .unwrap();
+
+        // Sync the wallet
+        wallet.sync(NoopProgress, None).unwrap();
+
+        // Start test setup
+        // tx1 = Coinbase -> core_reg_addr
+        // tx2 = tx1 -> ((bdk_addr, 4.0), (core_change_addr, 0.999))
+        // tx2 is a in wallet transaction, while tx1 is not
+        // trying to verify tx1 with RPCBlockchain will fail
+
+        // Get a regular core address
+        let core_reg_addr = core_rpc.get_new_address(None, None).unwrap();
+
+        // Send 5 btc to core regular address, no change
+        let tx1_id = core_rpc
+            .send_to_address(
+                &core_reg_addr,
+                Amount::from_btc(5.0).unwrap(),
+                None,
+                None,
+                Some(true),
+                None,
+                None,
+                None,
+            )
+            .unwrap();
+
+        // confirm tx1
+        core_rpc
+            .generate_to_address(1, &core_coinbase_addr)
+            .unwrap();
+
+        // Get the tx1 transaction data
+        let tx1 = core_rpc.get_transaction(&tx1_id, None).unwrap();
+        let tx1 = deserialize::<Transaction>(&tx1.hex).unwrap();
+
+        let bdk_addr = wallet.get_address(AddressIndex::New).unwrap();
+        let core_change_addrs = core_rpc.get_new_address(None, None).unwrap();
+
+        let mut output = HashMap::new();
+        output.insert(bdk_addr.to_string(), Amount::from_btc(4.0).unwrap());
+        output.insert(
+            core_change_addrs.to_string(),
+            Amount::from_btc(0.999).unwrap(),
+        );
+        let utxo = CreateRawTransactionInput {
+            txid: tx1_id,
+            vout: 0,
+            sequence: None,
+        };
+
+        let raw_tx_2 = core_rpc
+            .create_raw_transaction(&[utxo], &output, None, None)
+            .unwrap();
+        let signed_raw_tx_2 = core_rpc
+            .sign_raw_transaction_with_wallet(&raw_tx_2, None, None)
+            .unwrap();
+        let signed_tx_2 = deserialize::<Transaction>(&signed_raw_tx_2.hex).unwrap();
+
+        core_rpc.send_raw_transaction(&signed_tx_2).unwrap();
+        wallet.sync(NoopProgress, None).unwrap();
+
+        // This will fail
+        verify_tx(&tx1, &*wallet.database(), wallet.client()).unwrap();
+    }
 }
